71e6950ea369c30b4c4665432b5c6907b8033a77
motoakitakahashi <takahashi.motoaki@gmail.com>
2020-05-12 22:39:29 -0400

    fixed cost paid in composite goods

--- simulation_old.jl
+++ simulation_old.jl
@@ -9,32 +9,27 @@ using LinearAlgebra, Statistics, Compat
 N = 3 # number of countries
 σ = 4.0 # elasticity of substitution
 χ = 0.5 # labor force share in the Cobb-Douglas matching function
-z = [5, 5, 5] # productivity vector
+z = [10, 10, 10] # productivity vector
 ζ = [1, 1, 1] # vacancy cost vector
 f = [1, 1, 1] # entry cost vector
 t = [1 1.1 1.1; 1.1 1 1.1; 1.1 1.1 1] # trade cost matrix
 # in t, the rows are origins and the columns are destinations
 L = [1, 1, 1] # labor force vector
 
-function VLratio(Φ, P)
-    output = (σ - 1) ^ (1/χ) * (2*σ - 1) ^ (σ / (χ * (1-σ))) * Φ .^ (1/(σ-1)*χ) .* z .^ (1/χ) .* ζ .^ (-1/χ) .* f .^ (1/(χ * (1-σ))) .* P .^ (1/(χ * (1-σ)))
+function VLratio(Φ)
+    output = (σ - 1) ^ (1/χ) * (2*σ - 1) ^ (σ / (χ * (1-σ))) * Φ .^ (1/(σ-1)*χ) .* z .^ (1/χ) .* ζ .^ (-1/χ) .* f .^ (1/(χ * (1-σ)))
     return output
 end
 
-function expenditure(θ, M, P)
-    output = ζ .* θ .* L + M .* f .* P
+function expenditure(θ)
+    output = ζ .* θ .* L
     return output
 end
 
-function price_index(θ, M)
-    output1 = (2 * σ - 1) * (σ - 1) ^ (-1) * t' * (M .* ζ .* z .^ (-1) .* θ .^ χ)
+function price_index(Φ, θ)
+    output1 = (σ-1) .^ (-(σ+1)) * (2*σ-1) .^ (σ+1) * t' * (Φ .^ (-σ) .* ζ .^ (σ+1) .* z .^ (-σ) .* θ .^ (χ*σ+1) .* L)
     output2 = output1 .^ (1/(1-σ))
     return output2
-end
-
-function mass_of_firms(Φ, θ)
-    output = (2*σ-1)^σ * (σ-1)^(-σ) * Φ .^ (-σ) .* ζ .^ σ .* θ .^ (χ * (σ-1) + 1) .* z .^ (1-σ) .* L
-    return output
 end
 
 function market_potential(P, X)
@@ -47,7 +42,7 @@ end
 # If we work in a while loop, we cannot access variabled defined outside the while loop.
 # To deal with this issue, I wrap the while loop that computes an equilibrium with function.
 
-function equilibrium(Φ_guess, P_guess)
+function equilibrium(Φ_guess)
     # setup for the while loop
     maxit = 5000
     tol = 10 ^ (-6)
@@ -56,24 +51,17 @@ function equilibrium(Φ_guess, P_guess)
     dif = 1
 
     Φ = Φ_guess
-    P = P_guess
     θ = zeros(N, 1) # I need to write an object before a while loop...
     X = zeros(N, 1)
-    M = zeros(N, 1)
-
+    P = zeros(N, 1)
     while dif > tol && it < maxit
-        θ = VLratio(Φ, P)
-        M = mass_of_firms(Φ, θ)
-        X = expenditure(θ, M, P)
-        P = price_index(θ, M)
-        P_new = price_index(θ, M)
-        P_new = P_new ./ P_new[1] # the composite good in country 1 is the numeraire
-        Φ_new = market_potential(P_new, X)
+        θ = VLratio(Φ)
+        X = expenditure(θ)
+        P = price_index(Φ, θ)
+        Φ_new = market_potential(P, X)
 
-
-        dif = norm(([Φ_new; P_new] - [Φ; P]) ./ [Φ; P], Inf)
+        dif = norm((Φ_new - Φ)./Φ, Inf)
         Φ = λ * Φ_new + (1 - λ) * Φ
-        P = λ * P_new + (1 - λ) * P
         it = it + 1
     end
 
@@ -84,4 +72,4 @@ function equilibrium(Φ_guess, P_guess)
     return [Φ θ X P w u dif]
 end
 
-equilibrium([1, 1, 2], [1, 1, 1])
+equilibrium([1, 1, 2])
